cfc0ff5ceb3fde41f2831aa772f8e3c12ea73642
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-03-21 18:46:11 +0000

    Create addr2loc.sh

---
+++ addr2loc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+addresses="$HOME/.local/share/qmapshack/Addresses/addresses.csv"
+if [[ "$1" == "-g" ]] ; then
+	command -v osmconvert || ( echo "No osmconvert found in \$PATH" && exit )
+	command -v osmfilter || ( echo "No osmfilter found in \$PATH" && exit )
+	if ls addresses.csv > /dev/null 2>&1; then echo "addresses.csv exists in folder" && exit ; fi
+	temp=$(mktemp -d)
+	printf "%s\n" "Enter path to .pbf file"
+	read pbf
+	osmconvert "$pbf" -o="$temp"/XXX.xml --all-to-nodes --statistics
+	osmfilter "$temp"/XXX.xml --keep="place=city OR building OR addr* OR name OR amenity" --drop-author --drop-version -o="$temp"/xxx-buildg.xml
+	osmconvert "$temp"/xxx-buildg.xml -o="$temp"/result.csv --csv-headline --csv-separator=; --csv="@id addr:city addr:street addr:housenumber name amenity @lat @lon"
+	awk -F';' '$4' > addresses.csv
+	exit
+fi
+echo -e "Enter your query:\n(city?), street, house number"
+read query
+list=$(grep -i "$(echo "$query" | sed 's/[-, ]/.*/g')" "$addresses" | awk -F';' '$7 {print $2 ";" $3 ";" $4 ";" $7 ";" $8}')
+echo "$list" | sed 's/;\+/;/g;s/;/ | /g' | fzf | awk -F'|' '{print $(NF-1) $NF}' | sed 's/^ //;s/  / /'
