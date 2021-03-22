891ad77d6ba388a6cda103283d3c688f4d7545fa
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-03-22 14:01:31 +0000

    Update addr2loc.sh

--- addr2loc.sh
+++ addr2loc.sh
@@ -10,10 +10,11 @@ if [[ "$1" == "-g" ]] ; then
 	osmconvert "$pbf" -o="$temp"/XXX.xml --all-to-nodes --statistics
 	osmfilter "$temp"/XXX.xml --keep="place=city OR building OR addr* OR name OR amenity" --drop-author --drop-version -o="$temp"/xxx-buildg.xml
 	osmconvert "$temp"/xxx-buildg.xml -o="$temp"/result.csv --csv-headline --csv-separator=; --csv="@id addr:city addr:street addr:housenumber name amenity @lat @lon"
-	awk -F';' '$4' > addresses.csv
+	awk -F';' '$4 && $7' > addresses.csv
+	echo -e "now do:\nmkdir -p $(dirname ${addresses}) && cat addresses.csv >> \"$addresses\" && sort -u \"$addresses\" -o \"$addresses\""
 	exit
 fi
 echo -e "Enter your query:\n(city?), street, house number"
 read query
-list=$(grep -i "$(echo "$query" | sed 's/[-, ]/.*/g')" "$addresses" | awk -F';' '$7 {print $2 ";" $3 ";" $4 ";" $7 ";" $8}')
+list=$(grep -i "$(echo "$query" | sed 's/[-, ]/.*/g')" "$addresses" | awk -F';' '{print $2 ";" $3 ";" $4 ";" $7 ";" $8}')
 echo "$list" | sed 's/;\+/;/g;s/;/ | /g' | fzf | awk -F'|' '{print $(NF-1) $NF}' | sed 's/^ //;s/  / /'
